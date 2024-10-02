a8e1e294d73329febd9bc54e4fbea2d9b55d663c
soleil <forevergoodforest@outlook.com>
2024-10-02 13:52:22 +0800

    task2添加了eframe，并在main.rs写了个demo使用egui

--- task2/src/main.rs
+++ task2/src/main.rs
@@ -1,3 +1,24 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("My egui App", native_options, Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))))
+        .expect("Failed to run eframe native");
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Hello World!");
+       });
+   }
 }
